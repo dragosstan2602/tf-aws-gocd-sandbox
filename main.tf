2b102bcb7c6d5e83ab575de866080650a7d5393d
Dragos <dragos.stan2602@gmail.com>
2022-08-24 21:49:28 +0200

    removed all resources

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
-data "aws_availability_zones" "available" {
-  state = "available"
-}
+# data "aws_availability_zones" "available" {
+#   state = "available"
+# }
 
-resource "aws_vpc" "vpc01" {
-  cidr_block = "10.0.0.0/16"
+# resource "aws_vpc" "vpc01" {
+#   cidr_block = "10.0.0.0/16"
 
-  tags = {
-    "Name" = "euc1-terraform-vpc"
-  }
-}
+#   tags = {
+#     "Name" = "euc1-terraform-vpc"
+#   }
+# }
 
-resource "aws_subnet" "private_subnet" {
-  count             = 3
-  cidr_block        = "10.0.${count.index}.0/24"
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-  vpc_id            = aws_vpc.vpc01.id
+# resource "aws_subnet" "private_subnet" {
+#   count             = 3
+#   cidr_block        = "10.0.${count.index}.0/24"
+#   availability_zone = data.aws_availability_zones.available.names[count.index]
+#   vpc_id            = aws_vpc.vpc01.id
 
-  tags = {
-    "Name" = "private_subnet_${count.index + 1}"
-  }
-}
+#   tags = {
+#     "Name" = "private_subnet_${count.index + 1}"
+#   }
+# }
