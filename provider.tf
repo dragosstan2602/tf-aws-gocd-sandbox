c5adae6e2f5fac9fa3d03b1802db9e6471a5ac69
Dragos <dragos.stan2602@gmail.com>
2022-08-24 21:27:42 +0200

    changed provider config

--- provider.tf
+++ provider.tf
@@ -1,12 +1,18 @@
-terraform {  
-  required_providers {   
-    aws = {      
-      source  = "hashicorp/aws"      
-    }  
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+  backend "s3" {
+    bucket         = "tf-state-bucket-20220824"
+    key            = "gocd_state/tf-state.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "tf-state-db"
   }
 }
 
-provider "aws" {  
+provider "aws" {
   region = "eu-central-1"
 }
 
